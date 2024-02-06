114872223d1fa530feae99faee66b58be085da8e
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-02-06 18:07:44 -0300

    comecei a implementar funcionalidades ao nav

---
+++ index.js
@@ -0,0 +1,16 @@
+let before = document.getElementById("before-menu");
+let after = document.getElementById("after-menu");
+
+function beforeNav () {
+    before.style.display = 'none';
+    after.style.display = 'block';
+}
+
+function afterNav() {
+    after.style.display = 'none';
+    before.style.display = 'inline-block';
+}
+
+before.addEventListener('click', beforeNav);
+
+after.addEventListener('click', afterNav);
