57e8ffcc55d5fe21964f095f7b5edf06ee58dd08
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-02-19 18:16:54 -0300

    adicionei uma funcionalidade de busca

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ function hddNav() {
     }
 }
 
+
+document.getElementById('searchInput').addEventListener('input', function (event) {
+    const searchTerm = event.target.value.toLowerCase();
+    const listItems = document.querySelectorAll('#itemList li');
+
+    listItems.forEach(function (item) {
+        const itemText = item.textContent.toLowerCase();
+
+        if (itemText.includes(searchTerm)) {
+            item.style.display = 'list-item';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
+
 nav.addEventListener('click', hddNav);
 
 
